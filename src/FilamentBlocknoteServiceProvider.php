05ca1711c8300ffadaaa049dbb86e501822a03ba
Julius Kiekbusch <contact@julius-kiekbusch.de>
2023-10-27 10:35:14 +0200

    Format files

--- src/FilamentBlocknoteServiceProvider.php
+++ src/FilamentBlocknoteServiceProvider.php
@@ -2,17 +2,10 @@
 
 namespace Jubeki\FilamentBlocknote;
 
-use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use Jubeki\FilamentBlocknote\Commands\FilamentBlocknoteCommand;
-use Jubeki\FilamentBlocknote\Testing\TestsFilamentBlocknote;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -45,7 +38,7 @@ class FilamentBlocknoteServiceProvider extends PackageServiceProvider
      */
     protected function getAssets(): array
     {
-        $manifest = json_decode(file_get_contents(__DIR__ . '/../resources/dist/manifest.json'), true);
+        $manifest = json_decode(file_get_contents(__DIR__.'/../resources/dist/manifest.json'), true);
 
         return [
             Css::make(
@@ -61,8 +54,6 @@ class FilamentBlocknoteServiceProvider extends PackageServiceProvider
 
     protected function distFromManifest(array $manifest, string $key): string
     {
-        return __DIR__ . '/../resources/dist/' . $manifest[$key]['file'];
+        return __DIR__.'/../resources/dist/'.$manifest[$key]['file'];
     }
-
-
 }
